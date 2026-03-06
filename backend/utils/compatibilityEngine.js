405e93781fd77d1bda1b701a3cc587c4f6b63ea1
vidushi-kansal <vidushikansal08@gmail.com>
2026-03-06 22:10:36 +0530

    Update compatibilityEngine.js

--- backend/utils/compatibilityEngine.js
+++ backend/utils/compatibilityEngine.js
@@ -1,1 +1,20 @@
+function calculateCompatibility(user1, user2, weights) {
 
+    let score = 0;
+    let totalWeight = 0;
+
+    for (let key in weights) {
+
+        let diff = Math.abs(user1[key] - user2[key]);
+
+        let similarity = 1 - (diff / 5);
+
+        score += similarity * weights[key];
+
+        totalWeight += weights[key];
+    }
+
+    return (score / totalWeight) * 100;
+}
+
+module.exports = calculateCompatibility;
